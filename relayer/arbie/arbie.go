e877b2512589f57da9eb8a8735b8081e923f3a93
Andrej Hank <xhanka00@gmail.com>
2023-11-17 14:53:43 +0100

    Whitelisting more compulsory

--- relayer/arbie/arbie.go
+++ relayer/arbie/arbie.go
@@ -12,9 +12,48 @@ var MinPacketId = map[string]uint64{
 }
 
 var chainMap = map[string]string{
-	"SCRT": "secret-4",
-	"AXL":  "axelar-dojo-1",
-	"KUJI": "kaiyo-1",
+	"ATOM":    "cosmoshub-4",
+	"OSMO":    "osmosis-1",
+	"JUNO":    "juno-1",
+	"CRE":     "crescent-1",
+	"GRAV":    "gravity-bridge-3",
+	"BLD":     "agoric-3",
+	"CMDX":    "comdex-1",
+	"INJ":     "injective-1",
+	"AXL":     "axelar-dojo-1",
+	"LUNA":    "phoenix-1",
+	"LUNC":    "columbus-5",
+	"STRD":    "stride-1",
+	"KUJI":    "kaiyo-1",
+	"AKT":     "akashnet-2",
+	"BAND":    "laozi-mainnet",
+	"EVMOS":   "evmos_9001-2",
+	"MARS":    "mars-1",
+	"XPRT":    "core-1",
+	"STARS":   "stargaze-1",
+	"SCRT":    "secret-4",
+	"HUOBI":   "NO CHAIN ID",
+	"CANTO":   "canto_7700-1",
+	"KAVA":    "kava_2222-10",
+	"KAVAEVM": "kava_2222-10",
+	"ACRE":    "acre_9052-1",
+	"WHALE":   "migaloo-1",
+	"IRIS":    "irishub-1",
+	"HUAHUA":  "chihuahua-1",
+	"ORAI":    "Oraichain",
+	"NOBLE":   "noble-1",
+	"JKL":     "jackal-1",
+	"CRO":     "crypto-org-chain-mainnet-1",
+	"NTRN":    "neutron-1",
+	"QCK":     "quicksilver-2",
+	"UMEE":    "umee-1",
+	"ARCH":    "archway-1",
+	"SEI":     "pacific-1",
+	"BINANCE": "NO CHAIN ID",
+	"FET":     "fetchhub-4",
+	"TIA":     "celestia",
+	"SOMM":    "sommelier-3",
+	"DYDX":    "dydx-mainnet-1",
 }
 
 var Whitelist map[string][]string
@@ -68,7 +107,7 @@ func GetAccoutWhitelist() map[string][]string {
 		for chain, acct := range acctsMap {
 			chainId, ok := chainMap[chain]
 			if !ok {
-				continue
+				panic("chainId " + chainId + " not found")
 			}
 			if len(Whitelist[chainId]) == 0 {
 				Whitelist[chainId] = []string{}
